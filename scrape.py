471886378e60c39545f307b492215923b741e473
Amine Boukhari <amineboukhari20@mail.com>
2024-01-16 08:59:05 +0100

    test

--- scrape.py
+++ scrape.py
@@ -5,13 +5,31 @@ import os
 import sys
 
 
+# Function to read the current ID from the counter.txt file
+def read_counter(filename):
+    if os.path.exists(filename):
+        try:
+            with open(filename, 'r') as file:
+                current_id = int(file.read())
+        except ValueError:
+            current_id = 0  # Handle invalid data in the file
+    else:
+        current_id = 0  # Handle the case when the file doesn't exist
+    return current_id
+
+# Function to update and write the new ID to the counter.txt file
+def update_counter(filename, new_id):
+    with open(filename, 'w') as file:
+        file.write(str(new_id))
+
+
 #extract input name file from  command-line arguments
 if len(sys.argv) > 1:
     currentFile = sys.argv[1]
 
 #extract_all_text : extract text of given file 
 def extract_all_text(currentFile):
-    id_counter = 1  
+    id_counter = read_counter("counter.txt") 
     with open(currentFile, 'r', encoding='utf-8', errors='ignore') as file , open('extracted_text.json', 'r', encoding='utf-8', errors='ignore') as json_file :
         #html_content : our current page content 
         html_content = file.read()
@@ -68,13 +86,15 @@ def extract_all_text(currentFile):
             elif json_content[tag['data-id']] != tag.get_text().strip() :
                 json_content[tag['data-id']] = tag.get_text().strip()
 
-
+            
+        #update counter 
+        update_counter("counter.txt",id_counter)
 
     # or save to a file
-    with open('index.astro', 'w', encoding='utf-8') as file:
+    with open(currentFile, 'w', encoding='utf-8') as file:
         file.write(str(soup))
 
-    mergedJSON = {**extracted_data, **json_content}
+    mergedJSON = {**json_content,**extracted_data}
 
 
     with open("extracted_text.json", "w") as file:
